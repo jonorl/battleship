b91c357718f9de513a88e74747cc3817c393c2aa
jonorl <39122230+jonorl@users.noreply.github.com>
2024-12-05 17:27:03 +0300

    fix: Gameboard and Player logic to work with ship placement.

--- src/js/class.js
+++ src/js/class.js
@@ -1,6 +1,7 @@
 export class Ship {
-  constructor(shipLen, numberOfHits = 0, sunk = false) {
+  constructor(shipLen, direction, numberOfHits = 0, sunk = false) {
     this.shipLen = shipLen;
+    this.direction = direction;
     this.numberOfHits = numberOfHits;
     this.sunk = sunk;
   }
@@ -14,23 +15,61 @@ export class Ship {
   }
 }
 
-export class Player {
-  constructor(
-    real,
-    computer,
-    realGameboard = new Gameboard(10, 10),
-    computerGameboard = new Gameboard(10, 10)
-  ) {
-    this.real = real;
-    this.computer = computer;
-    this.realGameboard = realGameboard;
-    this.computerGameboard = computerGameboard;
-  }
-}
-
 export class Gameboard {
   constructor(axisX, axisY) {
     this.axisX = axisX;
     this.axisY = axisY;
+    this.board = this.createBoard();
+  }
+
+  createBoard() {
+    let horizontalLength = [];
+    for (let i = 0; i < this.axisX; i++) {
+      horizontalLength.push(i);
+    }
+    let verticalLength = [];
+    for (let i = 0; i < this.axisY; i++) {
+      verticalLength.push(i);
+    }
+    let board = [];
+    horizontalLength.forEach((xElement) => {
+      let arr = [];
+      board.push(arr);
+      verticalLength.forEach((yElement) => {
+        board[xElement].push(0);
+      });
+    });
+    // console.log(board)
+    return board;
+  }
+
+  placeShips(ship, coordinateX, coordinateY, player, board) {
+    if (ship.direction === "horizontal") {
+      if (!(coordinateX + ship.shipLen > this.axisX)) {
+        for (let i = coordinateX; i < (coordinateX + ship.shipLen); i++) {
+          if (player.playerType === true) {
+            board.board[coordinateY][i] = 1;
+          } else board.board[coordinateY][i] = 2;
+        }
+      } else return "Error placement out of bounds";
+    } else if (ship.direction === "vertical") {
+      if (!(coordinateY + ship.shipLen > this.axisY)) {
+        for (let i = coordinateY; i < (coordinateY + ship.shipLen); i++) {
+          if (player.playerType === true) {
+            board.board[i][coordinateX] = 1;
+          } else board.board[i][coordinateX] = 2;
+        }
+      } else return "Error placement out of bounds";
+    }
+    return board
+  }
+
+  receiveAttack(coordinates) {}
+}
+
+export class Player {
+  constructor(playerName, playerType = true) {
+    this.playerName = playerName;
+    this.playerType = playerType
   }
 }
--- src/js/class.test.js
+++ src/js/class.test.js
@@ -56,18 +56,6 @@ test("Ship(5) is not sunk after being hit 4 times test", () => {
   expect(ship.isSunk()).toBe(false);
 });
 
-// Player Class related testing:
-
-test("Player class should exist", () => {
-  const playerObj = new Player();
-  expect(playerObj).toBeInstanceOf(Player);
-});
-
-test("computer Player attribute should exist in Player", () => {
-  const playerObj = new Player("Jon", "CPU");
-  expect(playerObj.computer).toBeDefined();
-});
-
 // Gameboard Class related testing:
 
 test("Gameboard class should exist", () => {
@@ -78,4 +66,57 @@ test("Gameboard class should exist", () => {
 test("Y axis attribute should exist in Gameboard", () => {
   const gameboardObj = new Gameboard(10, 10);
   expect(gameboardObj.axisY).toBeDefined();
+});
+
+
+test("board creates a 10x10 grid", () => {
+  const gameboardObj = new Gameboard(10, 10);
+  expect(gameboardObj.axisX).toBe(10);
+  expect(gameboardObj.axisY).toBe(10);
+});
+
+test("board takes 10x10 values", () => {
+  let x = 10;
+  let y = 10;
+  const gameboardObj = new Gameboard(x, y);
+  const playerBoard = gameboardObj.createBoard();
+  expect(playerBoard[x-1].length).toBe(y);
+});
+
+test("Board places ship in coordinates", () => {
+
+  const gameboardObj = new Gameboard(10, 10);
+  const ship = new Ship(5, "horizontal")
+  const player = new Player("Jon")
+  player.human === true
+  expect(gameboardObj.placeShips(ship, 3, 4, player, gameboardObj)).toBeDefined();
+});
+
+test("Check if ships are in the right places (horizontal 5)", () => {
+
+  const gameboardObj = new Gameboard(10, 10);
+  const ship = new Ship(5, "horizontal");
+  const player = new Player("Jon");
+  player.human === true;
+  gameboardObj.placeShips(ship, 3, 4, player, gameboardObj)
+  expect(gameboardObj.board[3][4]).toBe(1);
+});
+
+test("Check if ships are in the right places (vertical 5)", () => {
+
+  const gameboardObj = new Gameboard(10, 10);
+  const ship = new Ship(5, "vertical");
+  const player = new Player("Jon");
+  player.human === true;
+  gameboardObj.placeShips(ship, 3, 4, player, gameboardObj)
+  console.log(gameboardObj.board)
+  expect(gameboardObj.board[3][4]).toBe(1);
+});
+
+
+// Player Class related testing:
+
+test("Player class should exist", () => {
+  const playerObj = new Player();
+  expect(playerObj).toBeInstanceOf(Player);
 });
