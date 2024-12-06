77d32a32ea68cf3991b4efc058df1ce924141719
jonorl <39122230+jonorl@users.noreply.github.com>
2024-12-06 14:57:35 +0300

    Fix: Gameboard and Player class logic plus added overlapping ships test

--- src/js/class.js
+++ src/js/class.js
@@ -43,35 +43,74 @@ export class Gameboard {
     return board;
   }
 
-  placeShips(ship, coordinateX, coordinateY, player, board) {
+  placeShips(ship, coordinateX, coordinateY, player) {
+    let validation = [];
     if (ship.direction === "horizontal") {
       if (coordinateX - 1 + ship.shipLen < this.axisX) {
         for (let i = coordinateX; i < coordinateX + ship.shipLen; i++) {
-          if (player.playerType === "human") {
-            board.board[coordinateY][i] = ship;
-          }
+          if (
+            typeof player.playerGameboard.board[coordinateY][i] !== "object"
+          ) {
+            // checking for ship overlap
+            validation.push(true);
+          } else validation.push(false);
+        }
+        if (
+          validation.every((item) => item === true) &&
+          validation.length !== 0
+        ) {
+          this.actualPlacingOfShips(ship, coordinateX, coordinateY, player);
+        } else if (
+          validation.some((item) => item === false) &&
+          validation.length !== 0
+        ) {
+          return "overlapping ships are not allowed";
         }
       } else return "Error placement out of bounds";
     } else if (ship.direction === "vertical") {
       if (coordinateY - 1 + ship.shipLen < this.axisY) {
         for (let i = coordinateY; i < coordinateY + ship.shipLen; i++) {
-          if (player.playerType === "human") {
-            board.board[i][coordinateX] = ship;
-          }
+          if (
+            typeof player.playerGameboard.board[i][coordinateX] !== "object"
+          ) {
+            validation.push(true);
+          } else validation.push(false);
         }
+      }
+      if (
+        validation.every((item) => item === true) &&
+        validation.length !== 0
+      ) {
+        this.actualPlacingOfShips(ship, coordinateX, coordinateY, player);
+      } else if (
+        validation.some((item) => item === false) &&
+        validation.length !== 0
+      ) {
+        return "overlapping ships are not allowed";
       } else return "Error placement out of bounds";
     }
-    return board;
+    return this.board;
+  }
+
+  actualPlacingOfShips(ship, coordinateX, coordinateY, player) {
+    if (ship.direction === "horizontal") {
+      for (let i = coordinateX; i < coordinateX + ship.shipLen; i++) {
+        player.playerGameboard.board[coordinateY][i] = ship;
+      }
+    } else if (ship.direction === "vertical") {
+      for (let i = coordinateY; i < coordinateY + ship.shipLen; i++) {
+        player.playerGameboard.board[i][coordinateX] = ship;
+      }
+    }
   }
 
   receiveAttack(x, y) {
     if (typeof this.board[y][x] === "object") {
       this.board[y][x].hit(); // this.board[y][x] is the same as ship
     } else this.board[y][x] = 1; // 0 for not yet hit, 1 for hit water
-    if (this.checkGameOver() === "Game Over"){
-      return "Game Over"
-    }
-    else return true;
+    if (this.checkGameOver() === "Game Over") {
+      return "Game Over";
+    } else return true;
   }
 
   checkGameOver() {
@@ -80,19 +119,20 @@ export class Gameboard {
       elementX.forEach((elementY) => {
         if (typeof elementY === "object" && elementY.sunk === true) {
           checkArray.push(true);
-        } else if(typeof elementY === "object" && elementY.sunk === false) checkArray.push(false);
+        } else if (typeof elementY === "object" && elementY.sunk === false)
+          checkArray.push(false);
       });
     });
-    if(checkArray.every(item => item === true)){
-      return "Game Over"
+    if (checkArray.every((item) => item === true)) {
+      return "Game Over";
     }
   }
 }
 
 export class Player {
-  constructor(playerName, playerType, playerGameboard = new Gameboard()) {
+  constructor(playerName, playerType, playerGameboard = new Gameboard(10, 10)) {
     this.playerName = playerName;
     this.playerType = playerType;
-    this.payerGameboard = playerGameboard;
+    this.playerGameboard = playerGameboard;
   }
 }
--- src/js/class.test.js
+++ src/js/class.test.js
@@ -59,18 +59,17 @@ test("Ship(5) is not sunk after being hit 4 times test", () => {
 // Gameboard Class related testing:
 
 test("Gameboard class should exist", () => {
-  const gameboardObj = new Gameboard();
+  const gameboardObj = new Gameboard(10 ,10);
   expect(gameboardObj).toBeInstanceOf(Gameboard);
 });
 
 test("Y axis attribute should exist in Gameboard", () => {
-  const gameboardObj = new Gameboard(10, 10);
+  const gameboardObj = new Gameboard(10 ,10);
   expect(gameboardObj.axisY).toBeDefined();
 });
 
-
 test("board creates a 10x10 grid", () => {
-  const gameboardObj = new Gameboard(10, 10);
+  const gameboardObj = new Gameboard(10 ,10);
   expect(gameboardObj.axisX).toBe(10);
   expect(gameboardObj.axisY).toBe(10);
 });
@@ -80,97 +79,94 @@ test("board takes 10x10 values", () => {
   let y = 10;
   const gameboardObj = new Gameboard(x, y);
   const playerBoard = gameboardObj.createBoard();
-  expect(playerBoard[x-1].length).toBe(y);
+  expect(playerBoard[x - 1].length).toBe(y);
 });
 
 test("Board places ship in coordinates", () => {
+  const ship = new Ship(5, "horizontal");
+  const player = new Player("Jon", "human");
+  expect(
+    player.playerGameboard.placeShips(ship, 3, 4, player)
+  ).toBeDefined();
+});
 
-  const gameboardObj = new Gameboard(10, 10);
-  const ship = new Ship(5, "horizontal")
-  const player = new Player("Jon", "human")
-  player.human === true
-  expect(gameboardObj.placeShips(ship, 3, 4, player, gameboardObj)).toBeDefined();
+test("Check if ship created returns out of bounds", () => {
+  const ship = new Ship(5, "vertical");
+  const player = new Player("Jon", "human");
+  expect(player.playerGameboard.placeShips(ship, 8, 6, player)).toBe(
+    "Error placement out of bounds"
+  );
 });
 
 test("Check if ships are in the right places (horizontal 5)", () => {
-
-  const gameboardObj = new Gameboard(10, 10);
   const ship = new Ship(5, "horizontal");
   const player = new Player("Jon", "human");
-  player.human === true;
-  gameboardObj.placeShips(ship, 3, 4, player, gameboardObj)
-  expect(gameboardObj.board[4][7]).toBeInstanceOf(Object);
+  player.playerGameboard.placeShips(ship, 3, 4, player);
+  expect(player.playerGameboard.board[4][3]).toBeInstanceOf(Object);
 });
 
 test("Check if ships are in the right places (vertical 5)", () => {
-
-  const gameboardObj = new Gameboard(10, 10);
   const ship = new Ship(5, "vertical");
   const player = new Player("Jon", "human");
-  player.human === true;
-  gameboardObj.placeShips(ship, 3, 4, player, gameboardObj)
-  expect(gameboardObj.board[8][3]).toBeInstanceOf(Object)
+  player.playerGameboard.placeShips(ship, 3, 4, player);
+  expect(player.playerGameboard.board[8][3]).toBeInstanceOf(Object);
+});
+
+test("Check if ship overlapping is allowed", () => {
+  const ship1 = new Ship(5, "vertical");
+  const ship2 = new Ship(5, "horizontal");
+  const player = new Player("Jon", "human");
+  player.playerGameboard.placeShips(ship1, 3, 4, player);
+  expect(player.playerGameboard.placeShips(ship2, 3, 4, player)).toBe('overlapping ships are not allowed');
 });
 
 test("Ship recieves attack", () => {
-
-  const gameboardObj = new Gameboard(10, 10);
   const ship = new Ship(5, "vertical");
   const player = new Player("Jon", "human");
-  player.human === true;
-  gameboardObj.placeShips(ship, 3, 4, player, gameboardObj)
-  gameboardObj.receiveAttack(3, 4)
-  expect(gameboardObj.receiveAttack(4, 3)).toBe(true);
+  player.playerGameboard.placeShips(ship, 3, 4, player);
+  player.playerGameboard.receiveAttack(3, 4);
+  expect(player.playerGameboard.receiveAttack(4, 3)).toBe(true);
 });
 
-
 test("Ship sinks after being hit shipLen times", () => {
-
-  const gameboardObj = new Gameboard(10, 10);
   const ship = new Ship(5, "vertical");
   const player = new Player("Jon", "human");
-  player.human === true;
-  gameboardObj.placeShips(ship, 3, 4, player, gameboardObj)
-  gameboardObj.receiveAttack(3, 4)
-  gameboardObj.receiveAttack(3, 5)
-  gameboardObj.receiveAttack(3, 6)
-  gameboardObj.receiveAttack(3, 7)
-  gameboardObj.receiveAttack(3, 8)
+  player.playerGameboard.placeShips(ship, 3, 4, player);
+  player.playerGameboard.receiveAttack(3, 4);
+  player.playerGameboard.receiveAttack(3, 5);
+  player.playerGameboard.receiveAttack(3, 6);
+  player.playerGameboard.receiveAttack(3, 7);
+  player.playerGameboard.receiveAttack(3, 8);
   expect(ship.isSunk()).toBe(true);
 });
 
 test("Trigger Game Over", () => {
-
-  const gameboardObj = new Gameboard(10, 10);
   const ship = new Ship(5, "vertical");
   const player = new Player("Jon", "human");
   player.human === true;
-  gameboardObj.placeShips(ship, 3, 4, player, gameboardObj)
-  gameboardObj.receiveAttack(3, 4)
-  gameboardObj.receiveAttack(3, 5)
-  gameboardObj.receiveAttack(3, 6)
-  gameboardObj.receiveAttack(3, 7)
-  gameboardObj.receiveAttack(3, 8)
-  expect(gameboardObj.receiveAttack(3, 8)).toBe("Game Over");
+  player.playerGameboard.placeShips(ship, 3, 4, player);
+  player.playerGameboard.receiveAttack(3, 4);
+  player.playerGameboard.receiveAttack(3, 5);
+  player.playerGameboard.receiveAttack(3, 6);
+  player.playerGameboard.receiveAttack(3, 7);
+  player.playerGameboard.receiveAttack(3, 8);
+  expect(player.playerGameboard.receiveAttack(3, 8)).toBe("Game Over");
 });
 
 test("More than one ship", () => {
-
-  const gameboardObj = new Gameboard(10, 10);
   const ship1 = new Ship(5, "vertical");
-  const ship2 = new Ship (2, "horizontal");
+  const ship2 = new Ship(2, "horizontal");
   const player = new Player("Jon", "human");
-  player.human === true;
-  gameboardObj.placeShips(ship1, 3, 4, player, gameboardObj)
-  gameboardObj.placeShips(ship2, 5, 7, player, gameboardObj)
-  gameboardObj.receiveAttack(3, 4)
-  gameboardObj.receiveAttack(3, 5)
-  gameboardObj.receiveAttack(3, 6)
-  gameboardObj.receiveAttack(3, 7)
-  gameboardObj.receiveAttack(3, 8)
-  gameboardObj.receiveAttack(5, 7)
-  gameboardObj.receiveAttack(6, 7)
-  expect(gameboardObj.receiveAttack(3, 8)).toBe("Game Over");
+  player.playerGameboard.placeShips(ship1, 3, 4, player);
+  player.playerGameboard.placeShips(ship2, 5, 7, player);
+  player.playerGameboard.receiveAttack(3, 4);
+  player.playerGameboard.receiveAttack(3, 5);
+  player.playerGameboard.receiveAttack(3, 6);
+  player.playerGameboard.receiveAttack(3, 7);
+  player.playerGameboard.receiveAttack(3, 8);
+  player.playerGameboard.receiveAttack(5, 7);
+  player.playerGameboard.receiveAttack(6, 7);
+  expect(player.playerGameboard.receiveAttack(3, 8)).toBe("Game Over");
 });
 
 // Player Class related testing:
